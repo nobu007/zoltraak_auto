f3319dc248430a968ec48681cf2568172df636dc
jinno <klvx01@gmail.com>
2024-09-27 23:27:06 +0900

    fix: zoltraak/utils/gui_util.py追加(コミット漏れ)

---
+++ zoltraak/utils/gui_util.py
@@ -0,0 +1,35 @@
+import pyperclip
+import tkinter
+
+
+class GuiUtil:
+    @staticmethod
+    def can_use_gui():
+        try:
+            # Tkinterのウィンドウを作成
+            root = tkinter.Tk()
+            root.withdraw()  # ウィンドウを表示しない
+            return True
+        except Exception:
+            return False
+        finally:
+            # ウィンドウを破棄
+            root.destroy()
+
+    @staticmethod
+    def copy_to_clipboard(text: str) -> bool:
+        if GuiUtil.can_use_gui():
+            pyperclip.copy(text)  # クリップボードにコピー
+            return True
+        return False
+
+    # pyautogui版は封印（importチェックすれば使えると思う）
+    # @staticmethod
+    # def can_use_gui() -> bool:
+    #     try:
+    #         # マウスの位置を取得してみる
+    #         _, _ = pyautogui.position()
+    #         return True
+    #     except Exception as e:
+    #         print("GUI環境が利用できません:", e)
+    #     return False
