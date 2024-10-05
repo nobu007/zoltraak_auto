624ba05836f1eb8b89b11992f8fb2e3583fa1887
jinno <klvx01@gmail.com>
2024-10-05 13:40:24 +0900

    fix: 呼び出し履歴が見づらいのでzoltraak.logを追加した

--- zoltraak/utils/log_util.py
+++ zoltraak/utils/log_util.py
@@ -1,7 +1,9 @@
 import functools
 import inspect
 import logging
+import os
 import sys
+from logging.handlers import RotatingFileHandler
 from typing import Any
 
 import zoltraak
@@ -11,15 +13,85 @@ default_level = logging.INFO if settings.is_debug else logging.WARNING
 logging.basicConfig(level=default_level)
 
 
+# logging.Formatter のフォーマット指定子
+#     %(name)s            ロガーの名前 (ロギングチャネル)
+#     %(levelno)s         メッセージの数値ロギングレベル (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+#     %(levelname)s       メッセージのテキストロギングレベル ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+#     %(pathname)s        ロギング呼び出しが発行されたソースファイルのフルパス名 (利用可能な場合)
+#     %(filename)s        パス名のファイル名部分
+#     %(module)s          モジュール (ファイル名の名前部分)
+#     %(lineno)d          ロギング呼び出しが発行されたソース行番号 (利用可能な場合)
+#     %(funcName)s        関数名
+#     %(created)f         LogRecord が作成された時刻 (time.time() の戻り値)
+#     %(asctime)s         LogRecord が作成された時刻のテキスト表現
+#     %(msecs)d           作成時刻のミリ秒部分
+#     %(relativeCreated)d ロギングモジュールがロードされた時刻からの相対ミリ秒数 (通常はアプリケーションの起動時)
+#     %(thread)d          スレッドID (利用可能な場合)
+#     %(threadName)s      スレッド名 (利用可能な場合)
+#     %(process)d         プロセスID (利用可能な場合)
+#     %(message)s         record.getMessage() の結果、レコードが発行される直前に計算される
+# PidFunctionFormatter.format() によるフォーマット指定子
+#     %(file_name)s       log関連を除いた呼び出し元のファイル名
+#     %(function_name)s   log関連を除いた呼び出し元の関数名
+
+FORMATTER_CONSOLE = "%(asctime)s - %(file_name)s - %(function_name)s - %(levelname)s - %(message)s"
+FORMATTER_WITH_PID = "%(asctime)s - PID:%(process)d - %(file_name)s - %(function_name)s - %(levelname)s - %(message)s"
+
+
 def get_logger(name: str, level: int = default_level) -> logging.Logger:
-    logger = logging.getLogger(name)
+    logger_ = logging.getLogger(name)
+    logger_.propagate = False
+    _add_handler(logger_, level=level)
+    _add_file_handler(logger_, level=level)
+    return logger_
+
+
+# ロギング呼び出し関数を取るときに対象外とするソースファイルの名前部分
+IGNORE_MODULES = ["__init__", "handlers", "log_util"]
+
+
+class PidFunctionFormatter(logging.Formatter):
+    def format(self, record):
+        # プロセスIDを追加（processでいいかも）
+        record.pid = os.getpid()
+        # デフォルトのfuncNameではlog_iなどの共通部分しか出ないため、呼び出し元の関数名を取得する
+        record.file_name, record.function_name = self.get_function_name()
+        return super().format(record)
+
+    def get_function_name(self) -> tuple[str, str]:
+        frame = inspect.currentframe()
+        while frame:
+            if frame is None:
+                return None
+            code = frame.f_code  # コードオブジェクトを取得
+            filename = os.path.basename(code.co_filename)
+            filename_without_ext = os.path.splitext(filename)[0]
+            # print("filename_without_ext=", filename_without_ext, ", code.co_name=", code.co_name)
+
+            if filename_without_ext not in IGNORE_MODULES:
+                return filename, code.co_name  # ファイル名と関数名を返す
+            frame = frame.f_back
+        return "unknown_file", "unknown_function"
+
+
+def _add_handler(logger_: logging.Logger, level: int = default_level) -> logging.Logger:
+    # コンソール出力用のハンドラの設定
     handler = logging.StreamHandler(stream=sys.stdout)
     handler.setLevel(level)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = PidFunctionFormatter(FORMATTER_CONSOLE)
     handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.propagate = False
-    return logger
+    logger_.addHandler(handler)
+
+
+def _add_file_handler(
+    logger_: logging.Logger, log_file: str = "zoltraak.log", level: int = default_level
+) -> logging.Logger:
+    # ファイルハンドラの設定
+    file_handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=5)  # 1MB, 最大5ファイル
+    file_handler.setLevel(level)
+    file_formatter = PidFunctionFormatter(FORMATTER_WITH_PID)
+    file_handler.setFormatter(file_formatter)
+    logger_.addHandler(file_handler)
 
 
 logger = get_logger(zoltraak.__name__)
@@ -39,10 +111,10 @@ def log_inout(func):
 def log_inout_info(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        logger = logging.getLogger(func.__name__)
-        logger.info(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
+        logger_ = logging.getLogger(func.__name__)
+        logger_.info(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
         result = func(*args, **kwargs)
-        logger.info(f"{func.__name__} returned: {result}")
+        logger_.info(f"{func.__name__} returned: {result}")
         return result
 
     return wrapper
@@ -51,10 +123,10 @@ def log_inout_info(func):
 def log_inout_debug(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        logger = logging.getLogger(func.__name__)
-        logger.debug(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
+        logger_ = logging.getLogger(func.__name__)
+        logger_.debug(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
         result = func(*args, **kwargs)
-        logger.debug(f"{func.__name__} returned: {result}")
+        logger_.debug(f"{func.__name__} returned: {result}")
         return result
 
     return wrapper
