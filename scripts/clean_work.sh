78108043ffa002fa85f3468d67ed0fc7848b33d9
jinno <klvx01@gmail.com>
2024-10-20 00:16:52 +0900

    fix: workディレクトリ初期化スクリプトを追加

---
+++ scripts/clean_work.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "CURRENT_DIR=$CURRENT_DIR"
+SAMPLES_DIR="$(cd "$CURRENT_DIR/../samples" && pwd)"
+echo "SAMPLES_DIR=$SAMPLES_DIR"
+WORK_DIR="$(cd "$CURRENT_DIR/../work" && pwd)"
+echo "WORK_DIR=$WORK_DIR"
+
+echo "rm -rf $WORK_DIR/*"
+rm -rf "$WORK_DIR/"*
+cp -p "$SAMPLES_DIR/destiny_def_InstantPromptBox.md" "$WORK_DIR/"
